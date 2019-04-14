5ea5e42bbf6ffdaa558c85c7a5ae14fa4ddc9eda
Daniel Berndt <daniel.berndt@gmail.com>
2019-04-14 15:44:33 +0200

    add first serious instruction set

---
+++ src/instructions/scouts.js
@@ -0,0 +1,100 @@
+import {createArrayWith} from "../lib/range";
+
+const scoutInstructions = {
+  startingRes: {food: 200, gold: 200, wood: 200, stone: 200},
+  ressources: {
+    berries: {type: "berries", count: 6, distance: 10},
+    sheep: {type: "sheep", count: 8, distance: 0},
+    deers: {type: "deer", count: 3, distance: 15},
+    strgl: {type: "stragglers", count: 6, distance: 4},
+    boars: {type: "boar", count: 2, distance: 15},
+    woodline1: {type: "wood", distance: 15},
+    woodline2: {type: "wood", distance: 20},
+    gold: {type: "gold", distance: 15},
+    stone: {type: "stone", distance: 15},
+  },
+  entites: {
+    tc: {type: "towncenter"},
+    v1: {type: "villager"},
+    v2: {type: "villager"},
+    v3: {type: "villager"},
+  },
+
+  tasks: {
+    v1: [
+      {type: "build", building: "house", distance: 1},
+      {type: "gather", resId: "sheep"},
+      {type: "gather", waitForEvent: "lureBoar1", resId: "boars"},
+    ],
+    v2: [
+      {type: "build", building: "house", distance: 1},
+      {type: "gather", resId: "sheep"},
+      {type: "gather", waitForEvent: "lureBoar1", resId: "boars"},
+    ],
+    v3: [
+      {type: "co-build", building: "house", distance: 1, with: ["v2"]},
+      {type: "gather", resId: "sheep"},
+      {type: "gather", waitForEvent: "lureBoar1", resId: "boars"},
+    ],
+    tc: [
+      ...createArrayWith(22 - 4, i => ({type: "create", unit: "villager", id: `v${i + 4}`})),
+      {type: "research", technology: "loom"},
+    ],
+    v4: [
+      {type: "gather", resId: "sheep"},
+      {type: "gather", waitForEvent: "lureBoar1", resId: "boars"},
+    ],
+    v5: [
+      {type: "gather", resId: "sheep"},
+      {type: "gather", waitForEvent: "lureBoar1", resId: "boars"},
+    ],
+    v6: [
+      {type: "gather", resId: "sheep"},
+      {type: "gather", waitForEvent: "lureBoar1", resId: "boars"},
+    ],
+    v7: [
+      {type: "build", building: "lumbercamp", nearRes: "woodline1"},
+      {type: "gather", resId: "woodline1"},
+    ],
+    v8: [{type: "gather", resId: "woodline1"}],
+    v9: [{type: "gather", resId: "woodline1"}],
+
+    v10: [
+      {type: "lure", boarId: "boars", onDone: "lureBoar1"},
+      {type: "gather", resId: "boars"},
+      {type: "build", building: "farm", id: "f1"},
+      {type: "farm", resId: "f1"},
+    ],
+
+    v11: [{type: "build", building: "house", distance: 8}, {type: "gather", resId: "berries"}],
+    v12: [{type: "build", building: "mill", atRes: "berries"}, {type: "gather", resId: "berries"}],
+    v13: [{type: "gather", resId: "berries"}],
+    v14: [{type: "gather", resId: "berries"}],
+
+    v15: [
+      {type: "lure", boarId: "boars"},
+      {type: "gather", resId: "boars"},
+      {type: "gather", resId: "sheep"},
+      {type: "build", building: "farm", waitForBuildRes: "farm", id: "f2"},
+      {type: "farm", resId: "f2"},
+    ],
+    v16: [
+      {type: "build", building: "house", distance: 8},
+      {type: "gather", resId: "boars"},
+      {type: "gather", resId: "sheep"},
+    ],
+    v17: [{type: "gather", resId: "boars"}, {type: "gather", resId: "sheep"}],
+    v18: [{type: "gather", resId: "boars"}, {type: "gather", resId: "sheep"}],
+
+    v19: [{type: "gather", resId: "woodline1"}],
+    v20: [{type: "gather", resId: "woodline1"}],
+    v21: [
+      {type: "build", building: "lumbercamp", nearRes: "woodline2"},
+      {type: "gather", resId: "woodline2"},
+    ],
+
+    // once boar2 is done, 3 on woodline2
+  },
+};
+
+export default scoutInstructions;
